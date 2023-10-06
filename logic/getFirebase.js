fc546cfabe5f535d1abeab50c0e2e3a0328bf542
mdg0410 <levoyersteven@gmail.com>
2023-10-06 15:58:28 -0500

    Update dates & Upgrade agent

--- logic/getFirebase.js
+++ logic/getFirebase.js
@@ -1,5 +1,5 @@
 const { firestorePromise } = require('../services/firebase/adminsdk.js');
-const { doc, getDoc, collection, getDocs } = require('firebase/firestore');
+const { doc, getDoc, collection, getDocs, query, where } = require('firebase/firestore');
 
 async function getDocument(coleccionNombre, documentoNombre) {
   try {
@@ -41,4 +41,30 @@ async function getCollection(coleccionNombre) {
 
 }
 
-module.exports = { getDocument, getCollection };
+async function queryCode(documentoNombre, codigo) {
+  try {
+    const firestore = await firestorePromise;
+    const colección = collection(firestore, 'productos');
+    const documento = doc(colección, documentoNombre);
+    const docSnapshot = await getDoc(documento);
+
+    if(docSnapshot.exists()){
+      const estantes = docSnapshot.data().datos;
+      const estanteEncontrado = estantes.find(estante => estante.code === codigo);
+      if (estanteEncontrado) {
+        return estanteEncontrado.media;
+      } else {
+        console.log(`No se encontró un estante con el código "${codigo}"`);
+        return null;
+      }
+    } else {
+      console.log('No se encontró el documento "Estantes" en la colección "productos"');
+      return null;
+    }
+  } catch (error) {
+    console.error(`Error al buscar el estante con el código "${codigo}":`, error);
+    throw error;
+  }
+}
+
+module.exports = { getDocument, getCollection, queryCode };
--- logic/setFirebase.js
+++ logic/setFirebase.js
@@ -1,13 +1,18 @@
 const { firestorePromise } = require('../services/firebase/adminsdk.js');
-const { collection, doc, setDoc } = require('firebase/firestore');
+const { collection, doc, setDoc, getDoc, updateDoc } = require('firebase/firestore');
 
-async function setClienteNumber(clienteId) {
+async function setClienteNumber(clienteId, nuevosDatos) {
   try {
     const firestore = await firestorePromise;
     const coleccion = collection(firestore, 'clientes');
     const nombreDocumento = clienteId;
     const documentoRef = doc(coleccion, nombreDocumento)
-    await setDoc(documentoRef, {});
+    const docSnap = await getDoc(documentoRef);
+    if (docSnap.exists()) {
+      await updateDoc(documentoRef, { datos: { ...docSnap.data().datos, ...nuevosDatos } });
+    } else {
+      await setDoc(documentoRef, { datos: nuevosDatos });
+    }
   } catch (error) {
     console.error(`Error al guardar el documento "${clienteId}" en la colección "clientes":`, error);
     throw error;
